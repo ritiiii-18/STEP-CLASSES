2c01cade51d537a2f595bc9ebfd529ac31b291bd
ritiiii-18 <ritishadixit17@gmail.com>
2026-03-17 13:08:03 +0530

    Create UC5.java

---
+++ UC5.java
@@ -0,0 +1,13 @@
+public class HelloArgs {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!"); // Default message
+        } else {
+            System.out.print("Hello");
+            for (String arg : args) {
+                System.out.print(", " + arg);
+            }
+            System.out.println("!");
+        }
+    }
+}
